0665ba3d9a59614432321f49ae971cadcf645ed8
haibuiminh <61405445+haibuiminh@users.noreply.github.com>
2021-06-11 09:42:49 +0700

    init project

---
+++ backend/app.js
@@ -0,0 +1,93 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const indexRouter = require('./routes/index');
+const adminRouter = require('./components/admins/router/adminRouter');
+const skillRouter = require('./components/skills/router/skillRouter');
+const levelRouter = require('./components/levels/router/levelRouter');
+const levelEducationRouter = require('./components/level-education/router/levelEducationRouter');
+const userRouter = require('./components/users/router/userRouter');
+const locationRouter = require('./components/locations/router/locationRouter');
+const contractRouter = require('./components/contracts/router/contractRouter');
+const complainRouter = require('./components/complain/router/complainRouter');
+const chatRouter = require('./components/chat/router/chatRouter');
+
+require('./components/utils/authentication/passport');
+const dbInfo = require('./components/utils/const/constant');
+
+const uri = dbInfo.CONNECTION_STRING;
+const app = express();
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('database connected');
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/apiadmin', indexRouter);
+app.use('/apiadmin/admin', adminRouter);
+app.use('/apiadmin/level', levelRouter);
+app.use('/apiadmin/level-education', levelEducationRouter);
+app.use('/apiadmin/location', locationRouter);
+app.use('/apiadmin/skill', skillRouter);
+app.use('/apiadmin/users', userRouter);
+app.use('/apiadmin/contract', contractRouter);
+app.use('/apiadmin/complain', complainRouter);
+app.use('/apiadmin/chat', chatRouter);
+
+app.get('/apiadmin/me', passport.authenticate('jwt'), (req, res, next) => {
+  res.send({
+    info: req.user.user
+  });
+});
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
