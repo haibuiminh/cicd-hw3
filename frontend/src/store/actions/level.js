76fa94b92549d5c2efb179fbfb292b98bb1f2d6f
DESKTOP-C0L9UHR\vunam <vunam1218@gmail.com>
2021-06-12 19:57:15 +0700

    update call api frontend

--- frontend/src/store/actions/level.js
+++ frontend/src/store/actions/level.js
@@ -80,7 +80,7 @@ export const updateLevel = (_id, title) => dispatch => {
     'Content-Type': 'application/json',
     Authorization: 'Bearer ' + authToken
   };
-  const url = `http://localhost:4200/level/update-level`;
+  const url = `/api/level/update-level`;
   axios
     .put(url, updated, {
       headers
@@ -125,7 +125,7 @@ export const getListLevel = searchString => dispatch => {
     Authorization: 'Bearer ' + authToken
   };
   const resultSearch = searchString ? `?searchString=${searchString}` : '';
-  const url = `http://localhost:4200}/level/get-list${resultSearch}`;
+  const url = `/api}/level/get-list${resultSearch}`;
   axios
     .get(url, {
       headers
@@ -167,7 +167,7 @@ export const deleteLevel = _id => dispatch => {
     'Content-Type': 'application/json',
     Authorization: 'Bearer ' + authToken
   };
-  const url = `http://localhost:4200/level/delete-level`;
+  const url = `/api/level/delete-level`;
   axios
     .delete(url, {
       headers,
